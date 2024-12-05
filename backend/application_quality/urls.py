72aa53f3ba3a9cda034abf83863e3d784d7e5d05
Hugo Cremers <hugo.cremers@spaceapplications.com>
2024-12-05 17:40:41 +0100

    enabling oidc according to env var

--- backend/application_quality/urls.py
+++ backend/application_quality/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from rest_framework.authtoken.views import ObtainAuthToken
+import os
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,5 +27,7 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("api/token-auth/", ObtainAuthToken.as_view(), name="token-auth"),
-    path("oidc/", include("mozilla_django_oidc.urls")),
 ]
+
+if os.getenv("OIDC_ENABLED") in ["true", "True"]:
+    urlpatterns.append(path("oidc/", include("mozilla_django_oidc.urls")))
--- backend/backend/auth_backends.py
+++ backend/backend/auth_backends.py
@@ -1,11 +1,15 @@
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class CustomOIDCAuthenticationBackend(OIDCAuthenticationBackend):
     def create_user(self, claims):
         user = super().create_user(claims)
-        user.first_name = claims.get('given_name', '')
-        user.last_name = claims.get('family_name', '')
+        user.first_name = claims.get("given_name", "")
+        user.last_name = claims.get("family_name", "")
         user.username = claims.get("preferred_username", claims["sub"])
+        logger.info(f"Creating user with username '{user.username}'")
         user.save()
         return user
