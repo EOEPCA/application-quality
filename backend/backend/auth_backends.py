7021eed29132a0666bf344a1a779e71e197e4a6c
Bernard Valentin <bernard.valentin@gmail.com>
2025-01-15 01:27:51 +0100

    Create an example address if no email present in OIDC user claims

--- backend/backend/auth_backends.py
+++ backend/backend/auth_backends.py
@@ -1,17 +1,27 @@
+import logging
+
 from django.conf import settings
 from django.utils.crypto import get_random_string
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
-import logging
+
 
 logger = logging.getLogger(__name__)
 
 
 class CustomOIDCAuthenticationBackend(OIDCAuthenticationBackend):
+
+    def verify_claims(self, claims):
+        if "email" not in claims:
+            username = claims.get("preferred_username", claims["sub"])
+            claims["email"] = f"{username}@example.com"
+        logger.info(f"Claims to verify: {claims}")
+        return super().verify_claims(claims)
+
     def create_user(self, claims):
         user = super().create_user(claims)
-        user.first_name = claims.get("given_name", "")
-        user.last_name = claims.get("family_name", "")
         user.username = claims.get("preferred_username", claims["sub"])
+        user.first_name = claims.get("given_name", user.username)
+        user.last_name = claims.get("family_name", "")
         logger.info(f"Creating user with username '{user.username}'")
         user.save()
         return user
