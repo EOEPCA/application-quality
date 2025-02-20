5a8002e7ce703672f9b81f23a6e49d1df546ffc8
Bernard Valentin <bernard.valentin@spaceapplications.com>
2025-02-20 13:16:58 +0100

    Initial frontend testing

---
+++ frontend/vitest.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitest/config'
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    root: fileURLToPath(new URL('./test', import.meta.url)),
+    server: {
+      deps: {
+        inline: ['vuetify']
+      }
+    },
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => {
+              return tag.startsWith('v-') 
+            }
+          }
+        }
+      })
+    ],
+  }
+})
