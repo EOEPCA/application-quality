5cf7c0866e0e05b60f8600dcdbe3b11ea03ef36a
Hugo Cremers <hugo.cremers@spaceapplications.com>
2025-03-28 14:57:40 +0100

    install kubectl and vcluster from Dockerfile

--- backend/vcluster.sh
+++ backend/vcluster.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-apt-get update && apt-get install -y curl # vim
-
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/download/v0.24.0/vcluster-linux-amd64" && install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
-vcluster -v
-
 kubectl config set-cluster in-cluster \
   --server="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}" \
   --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt \
