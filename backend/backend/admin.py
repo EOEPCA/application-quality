3d2b22e18283b888efe7a3e03315416276b658c4
Hugo Cremers <hugo.cremers@spaceapplications.com>
2025-07-02 16:29:32 +0200

    added list_displays in django admin pages

--- backend/backend/admin.py
+++ backend/backend/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from backend import models
 
-admin.site.register(models.Pipeline)
-admin.site.register(models.PipelineRun)
-admin.site.register(models.JobReport)
+class PipelineSettings(admin.ModelAdmin):
+    list_display = ("name", "description", "owner", "version", "created_at", "edited_at")
+
+class PipelineRunSettings(admin.ModelAdmin):
+    list_display = ("__str__", "pipeline", "start_time", "completion_time", "status", "user", "started_by")
+
+class ToolSettings(admin.ModelAdmin):
+    list_display = ("slug", "name", "version")
+
+class CommandSettings(admin.ModelAdmin):
+    list_display = ("slug", "name", "version")
+
+class JobReportSettings(admin.ModelAdmin):
+    list_display = ("__str__", "name", "created_at")
+
+admin.site.register(models.Pipeline, PipelineSettings)
+admin.site.register(models.PipelineRun, PipelineRunSettings)
+admin.site.register(models.Subworkflow, ToolSettings)
+admin.site.register(models.CommandLineTool, CommandSettings)
+admin.site.register(models.JobReport, JobReportSettings)
 admin.site.register(models.Tag)
-admin.site.register(models.Subworkflow)
-admin.site.register(models.CommandLineTool)
